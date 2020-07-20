cbf95f0f63c707b20aa46f25c72d5359d83b2046
emabytes <emadavor22@gmail.com>
2020-07-20 11:53:44 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// • Formel für Menschen mit einem zierlichen und schmalen Körperbau:
+// [(Körpergröße in cm - 100) + (Alter/ 10)] x 0,9 x 0,9
+ //weight = [(height - 100) + (age/10)] * 0.9 * 0.9;
+
+
+// • Formel für Menschen mit einem eher breiten Körperbau:
+// [(Körpergröße in cm - 100) + (Alter/ 10)] x 0,9 x 1,1
+ //weight = [(height - 100) + (age/10)] * 0.9 * 1.1;
+
+
+function calculateWeight() {
+    let height = Number(document.getElementById("height").value);
+    let age = Number(document.getElementById("age").value);
+    let smaller = document.getElementById("smaller").checked;
+    let bigger = document.getElementById("bigger").checked;
+    let weight;
+
+    if (bigger == true) {
+        weight = [(height - 100) + (age / 10)] * 0.9 * 1.1;
+        console.log ("bigger" + weight)
+    } else if (smaller == true) {
+        weight = [(height - 100) + (age / 10)] * 0.9 * 0.9;
+        console.log("smaller" + weight);
+    }
+    document.getElementById("result").innerHTML = Math.floor(weight);
+}
